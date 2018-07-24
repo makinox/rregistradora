047cba02f11cd7efa667e9e4bed7daee703ce607
Jesus Bossa <jesusbossa@protonmail.com>
2018-07-24 10:03:05 -0500

    index section 1 improved

--- src/components/index/section1/index.js
+++ src/components/index/section1/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import img1 from './../../../assets/index/section1/img1.jpg'
 import './index.css'
 
 export default class App extends Component {
@@ -6,16 +7,18 @@ export default class App extends Component {
         return (
             <section className="section-1">
                 <div className="container">
-                    <div className="row row-section1">
-                        <div className="col col-12 col-md-6">
+                    <div className="row">
+                        <div className="col col-12 col-md-6 col-section1">
                             <h1>Registradoras y equipos del caribe</h1>
-                            {/* <h3>Distribuidores de registradoras y equipos a nivel internacional</h3> */}
-                            <p>Exportamos maquinas y sofware financiero para cualquier tipo de empresa, en cualquier parte del mundo</p>
-                            <a href="jsx-a11y/href-no-hash" className="btn btn-outline-info">Explora</a>
-                            <a href="jsx-a11y/href-no-hash" className="btn btn-info">Cuentanos como podemos ayudarte</a>
+                            <p>Distribuidores de registradoras y equipos a nivel internacional.
+                                <br/>
+                                Exportamos maquinas y sofware financiero para cualquier tipo de empresa, en cualquier parte del mundo.
+                            </p>
+                            <a href="jsx-a11y/href-no-hash" className="m-1 btn btn-outline-info">Explora</a>
+                            <a href="jsx-a11y/href-no-hash" className="m-1 btn btn-info">Cuentanos como podemos ayudarte</a>
                         </div>
-                        <div className="col col-12 col-md-6">
-                            <h3>Distribuidores de registradoras y equipos a nivel internacional</h3>
+                        <div className="col col-12 col-md-6 pt-4">
+                            <img src={ img1 } alt="Negocios utilizando regypos" className="rounded-circle" width="600" height="600"/>
                         </div>
                     </div>
                 </div>
